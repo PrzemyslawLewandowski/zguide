968a3e14050128052c84b54b144c29bc8e1f8478
Przemyslaw Lewandowski <przemyslaw.lewandowski@roche.com>
2012-06-20 10:18:54 +0200

    Added hello world C# projects.

---
+++ examples/C#/exampleProjects/HelloWorld/HelloWorldServer/HelloWorldServer/Program.cs
@@ -0,0 +1,42 @@
+﻿//
+//  Hello World server
+//  Binds REP socket to tcp://*:5555
+//  Expects "Hello" from client, replies with "World"
+//
+
+//  Author:     Michael Compton, Tomas Roos, Przemyslaw Lewandowski
+//  Email:      michael.compton@littleedge.co.uk, ptomasroos@gmail.com, prz.lewandowski@gmail.com
+
+using System;
+using System.Text;
+using System.Threading;
+using ZeroMQ;
+
+namespace ZMQGuide
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = ZmqContext.Create())
+            using (var replyer = context.CreateSocket(SocketType.REP))
+            {
+                replyer.Bind("tcp://*:5555");
+
+                const string replyMessage = "World";
+
+                while (true)
+                {
+                    string message = replyer.Receive(Encoding.Unicode);
+                    Console.WriteLine("Received request: {0}", message);
+
+                    // Simulate work, by sleeping
+                    Thread.Sleep(1000);
+
+                    // Send reply back to client
+                    replyer.Send(replyMessage, Encoding.Unicode);
+                }
+            }
+        }
+    }
+}
